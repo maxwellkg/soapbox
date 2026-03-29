880065e6e2a66577195abc668c5b2afd59d90fba
Maxwell Gove <maxwell.gove@hey.com>
2026-03-28 21:00:12 -0400

    add columns to authors table

--- db/migrate/20260328223805_create_authors.rb
+++ db/migrate/20260328223805_create_authors.rb
@@ -1,6 +1,8 @@
 class CreateAuthors < ActiveRecord::Migration[8.1]
   def change
     create_table :authors do |t|
+      t.string :first_name, null: false
+      t.string :last_name, null: false
       t.string :email_address, null: false
       t.string :password_digest, null: false
       t.boolean :singleton_guard, null: false, default: true
@@ -8,7 +10,6 @@ class CreateAuthors < ActiveRecord::Migration[8.1]
       t.timestamps
     end
 
-    add_index :authors, :email_address, unique: true
     add_index :authors, :singleton_guard, unique: true
     add_check_constraint :authors, "singleton_guard = 1", name: "authors_singleton_guard_true"
   end
