f210ea37f243e0e18b9971c864e01e68d46e0754
Maxwell Gove <maxwell.gove@hey.com>
2026-03-28 20:59:23 -0400

    updates to authors

--- app/models/author.rb
+++ app/models/author.rb
@@ -7,10 +7,9 @@ class Author < ApplicationRecord
   validate :single_instance_only, on: :create
   validates :first_name, presence: true
   validates :last_name, presence: true
-  
+
   validates :email_address,
             presence: true,
-            uniqueness: true,
             format: { with: URI::MailTo::EMAIL_REGEXP }  
 
   class << self
@@ -25,6 +24,10 @@ class Author < ApplicationRecord
     def instance?
       exists?
     end
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
   end
 
   private
