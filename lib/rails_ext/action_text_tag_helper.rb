6e595430518cebdcb61a01d3e2184a8199dc3548
Maxwell Gove <maxwell.gove@hey.com>
2026-08-14 20:16:34 -0400

    styling updates

--- lib/rails_ext/action_text_tag_helper.rb
+++ lib/rails_ext/action_text_tag_helper.rb
@@ -14,19 +14,6 @@ module ActionText
     def house_toolbar(**options, &block)
       tag.house_md_toolbar(**options, &block)
     end
-
-    def house_toolbar_button(action, **options, &block)
-      tag.button title: action.to_s.humanize, data: { "house-md-action": action }, **options, &block
-    end
-
-    def house_toolbar_file_upload_button(name: "upload", title: "Upload File", **options, &block)
-      tag.label title: title, **options do
-        safe_join [
-          file_field_tag(name, data: { "house-md-toolbar-file-picker": true }, style: "display: none;"),
-          capture(&block)
-        ]
-      end
-    end
   end
 end
 
